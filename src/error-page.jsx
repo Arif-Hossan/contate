ab3f1be63b0162affcf721a83e5e981a724940bd
Arif Hossan <arif.hossan.eu@gmail.com>
2023-05-03 01:05:56 +0600

    simple layout and some data show without rendering the page

---
+++ src/error-page.jsx
@@ -0,0 +1,15 @@
+import { useRouteError } from "react-router-dom";
+export default function ErrorPage(){
+    const error = useRouteError();
+    console.log(error);
+
+    return (
+        <div id="error-page">
+            <h1>Opps</h1>
+            <p>Something Error Happen</p>
+            <p>
+                <i>{error.statusText || error.message}</i>
+            </p>
+        </div>
+    );
+}
